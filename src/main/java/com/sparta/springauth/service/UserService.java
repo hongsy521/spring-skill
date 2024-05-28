ea5a3c9d61ec79ecacfebbf118c2f366864bc83b
hongsy521 <seoyeong0521@naver.com>
2024-05-28 13:51:08 +0900

    Spring Security : JWT 로그인 // 비즈니스 로직상에서의 인증,인가 필터단으로 이동

--- src/main/java/com/sparta/springauth/service/UserService.java
+++ src/main/java/com/sparta/springauth/service/UserService.java
@@ -59,20 +59,5 @@ public class UserService {
         userRepository.save(user);
     }
 
-    public void login(LoginRequestDto requestDto, HttpServletResponse res) {
-        String username = requestDto.getUsername();
-        String password = requestDto.getPassword();
 
-        // 사용자 확인
-        User user = userRepository.findByUsername(username).orElseThrow(()->new IllegalArgumentException("등록된 사용자가 없습니다."));
-
-        // 비밀번호 확인
-        if(!passwordEncoder.matches(password,user.getPassword())){
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-        }
-
-        // JWT 생성 후 쿠키에 저장하여 response 객체에 추가해주기
-        String token = jwtUtil.createToken(user.getUsername(),user.getRole());
-        jwtUtil.addJwtToCookie(token, res);
-    }
 }
